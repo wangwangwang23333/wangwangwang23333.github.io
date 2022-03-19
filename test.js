65e65f585cb28f60e5257810bf56fe2522a01246
汪汪汪 <1209497940@qq.com>
2022-03-19 17:11:21 +0800

    Site updated: 2022-03-19 17:11:16

---
+++ test.js
@@ -0,0 +1,3 @@
+var str = '1';
+var str2 = str; //str、str2 是 string 类型
+console.log(typeof str2);
---
+++ test.ts
@@ -0,0 +1,3 @@
+var str = '1' 
+var str2:number = <number> <any> str   //str、str2 是 string 类型
+console.log(typeof str2)
